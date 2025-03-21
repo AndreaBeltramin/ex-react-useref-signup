88792082287fee3df72b460be28c40f780fa32d0
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-21 18:04:22 +0100

    aggiunta controlli

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const letters = "abcdefghijklmnopqrstuvwxyz";
+	const numbers = "0123456789";
+	const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
+
 	const [name, setName] = useState("");
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
@@ -9,6 +13,12 @@ function App() {
 	const [anni, setAnni] = useState("");
 	const [descrizione, setDescrizione] = useState("");
 
+	const isUsernameValid =
+		username.length >= 6 && /^[a-zA-Z0-9]+$/.test(username);
+	const isPasswordValid =
+		password.length >= 8 && /^[a-zA-Z0-9]+$/.test(password);
+	const isDescrizioneValid =
+		descrizione.trim().length >= 100 && descrizione.trim().length <= 1000;
 	const handleChange = (e) => {
 		const option = e.target.value;
 		setSpecializzazione(option);
@@ -57,6 +67,13 @@ function App() {
 						placeholder="Inserisci il tuo username"
 						onChange={(e) => setUsername(e.target.value)}
 					/>
+					{username && (
+						<p style={{ color: isUsernameValid ? "green" : "red" }}>
+							{isUsernameValid
+								? "Username valido"
+								: "Deve contenere 6 caratteri e non può contenere simboli"}
+						</p>
+					)}
 				</div>
 
 				<div className="input">
@@ -69,6 +86,13 @@ function App() {
 						placeholder="Inserisci la tua password"
 						onChange={(e) => setPassword(e.target.value)}
 					/>
+					{password && (
+						<p style={{ color: isPasswordValid ? "green" : "red" }}>
+							{isPasswordValid
+								? "Password valida"
+								: "Deve contenere 6 caratteri e non può contenere simboli"}
+						</p>
+					)}
 				</div>
 				<div className="input">
 					<label htmlFor="specializzazione">
@@ -101,7 +125,7 @@ function App() {
 
 				<div className="input">
 					<label htmlFor="descrizione">Breve descrizione: </label>
-					<input
+					<textarea
 						required
 						type="textarea"
 						id="descrizione"
@@ -109,6 +133,13 @@ function App() {
 						placeholder="Inserisci una breve descrizione"
 						onChange={(e) => setDescrizione(e.target.value)}
 					/>
+					{descrizione && (
+						<p style={{ color: isDescrizioneValid ? "green" : "red" }}>
+							{isDescrizioneValid
+								? "Descrizione valida"
+								: "Deve contenere più di 100 caratteri ma meno di 1000"}
+						</p>
+					)}
 				</div>
 				<button type="submit">Conferma dati</button>
 			</form>
